eec683a6e18a1997eb75249a51600443f2344d22
sebastien lapratte <va2dos@gmail.com>
2025-09-09 19:22:33 -0400

    Initial Class

---
+++ Tp1Program.java
@@ -0,0 +1,9 @@
+/*
+ * McGill - Programing 1 - TP 1
+ */
+public class Tp1Program {
+    public static void main(String[] args) { 
+       // Display message Welcome to Java! on the console
+       System.out.println("Welcome to Java!");
+     }
+}
